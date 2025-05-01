34fb031a9cd16c877886900b78b0c7de5ed76fee
kozraf <kozraf@gmail.com>
2025-05-01 23:40:51 +0100

    Fixing codebuild.tf

--- bootstrap/codebuild.tf
+++ bootstrap/codebuild.tf
@@ -1,3 +1,25 @@
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-eks-observability-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_admin" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 resource "aws_codebuild_project" "eks_monitoring" {
   name          = "eks-observability-demo"
   service_role  = aws_iam_role.codebuild_role.arn
