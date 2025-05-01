b1682a84c33929f2509a526654b887260727172a
kozraf <kozraf@gmail.com>
2025-05-01 23:35:18 +0100

    Pushing codebuild.tf to use Github with Webhooks

--- bootstrap/codebuild.tf
+++ bootstrap/codebuild.tf
@@ -1,30 +1,3 @@
-variable "github_repo_url" {}
-variable "region" {
-  default = "us-east-1"
-}
-
-resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-eks-observability-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_admin" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
 resource "aws_codebuild_project" "eks_monitoring" {
   name          = "eks-observability-demo"
   service_role  = aws_iam_role.codebuild_role.arn
@@ -32,26 +5,18 @@ resource "aws_codebuild_project" "eks_monitoring" {
   build_timeout = 20
 
   source {
-    type      = "GITHUB"
-    location  = var.github_repo_url
-    buildspec = "terraform/buildspec.yml"
+    type            = "GITHUB"
+    location        = var.github_repo_url
+    buildspec       = "terraform/buildspec.yml"
     git_clone_depth = 1
-
     git_submodules_config {
       fetch_submodules = false
     }
-  }
-
-  triggers {
-    webhook = true
-    filter_groups = [
-      [
-        {
-          type    = "EVENT"
-          pattern = "PUSH"
-        }
-      ]
-    ]
+    git_config {
+      fetch_submodules = false
+    }
+    report_build_status = true
+    webhook             = true
   }
 
   environment {
