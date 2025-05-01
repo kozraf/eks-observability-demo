6a6a75cb138aebf9f66e1dcad625df612d296486
kozraf <kozraf@gmail.com>
2025-05-01 23:26:14 +0100

    Pushing deploy.sh for Helm

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Add Helm repos
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add kubecost https://kubecost.github.io/cost-analyzer/
+helm repo add podinfo https://stefanprodan.github.io/podinfo
+helm repo update
+
+# Deploy Prometheus stack
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+  --namespace monitoring --create-namespace
+
+# Deploy Kubecost
+helm upgrade --install kubecost kubecost/cost-analyzer \
+  --namespace kubecost --create-namespace \
+  --set kubecostToken="demo"
+
+# Deploy sample pod (podinfo)
+helm upgrade --install podinfo podinfo/podinfo \
+  --namespace apps --create-namespace
