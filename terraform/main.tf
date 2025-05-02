569b50249c81f00776611b77d6d4cd22a5e619f1
kozraf <kozraf@gmail.com>
2025-05-02 17:13:33 +0100

    configuring IAM role to have access to kubectl

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,16 +8,9 @@ terraform {
   }
 }
 
-############################
-# Provider
-############################
-provider "aws" {
-  region = var.region
-}
+provider "aws" { region = var.region }
 
-############################
-# VPC
-############################
+################ VPC ################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -37,12 +30,9 @@ module "vpc" {
   }
 }
 
-############################
-# IAM role for cluster admins
-############################
+############### IAM admin role ###############
 resource "aws_iam_role" "eks_admin" {
   name = "eks-admin-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -52,24 +42,24 @@ resource "aws_iam_role" "eks_admin" {
     }]
   })
 }
-
 resource "aws_iam_role_policy_attachment" "eks_admin_attach" {
   role       = aws_iam_role.eks_admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-############################
-# EKS cluster + node group
-############################
+#################### EKS ####################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.36.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.30"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  ### >>> allow CodeBuild to reach the API ###
+  cluster_endpoint_public_access = true
+  ###########################################
 
   enable_irsa = true
 
@@ -82,55 +72,28 @@ module "eks" {
     }
   }
 
-  tags = {
-    Environment = "demo"
-  }
+  tags = { Environment = "demo" }
 }
 
-###############################################################################
-# Providers that connect to the brand‑new cluster
-###############################################################################
+############## Providers wired to the new cluster ##############
+data "aws_eks_cluster_auth" "this" { name = module.eks.cluster_name }
 
-# 1. Get a short‑lived auth token for this cluster
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-# 2. Kubernetes provider (aliased “eks”) that the aws‑auth module will use
 provider "kubernetes" {
-  alias                  = "eks"
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  alias                   = "eks"
+  host                    = module.eks.cluster_endpoint
+  cluster_ca_certificate  = base64decode(module.eks.cluster_certificate_authority_data)
+  token                   = data.aws_eks_cluster_auth.this.token
 }
 
-# 3. Helm provider (optional but handy for the deploy.sh step)
-provider "helm" {
-  alias                  = "eks"
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-
-############################
-# aws‑auth ConfigMap (sub‑module)
-############################
+################ aws‑auth ConfigMap ################
 module "aws_auth" {
   source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version = "20.36.0"
 
-  depends_on = [module.eks]
+  providers = { kubernetes = kubernetes.eks }
 
-  providers = {
-    kubernetes = kubernetes.eks   # <‑‑ use the alias defined above
-  }
-
-  # *this* is the only flag you need
   manage_aws_auth_configmap = true
 
-  # still uses the v19‑style names
   aws_auth_roles = [
     {
       rolearn  = aws_iam_role.eks_admin.arn
