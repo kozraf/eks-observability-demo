33f0635d210e7334bd949b3740d5451ab985a073
kozraf <kozraf@gmail.com>
2025-05-02 17:55:14 +0100

    configuring IAM role to have access to kubectl

--- bootstrap/bootstrap_codebuild.tf
+++ bootstrap/bootstrap_codebuild.tf
@@ -51,7 +51,11 @@ resource "aws_dynamodb_table" "lock" {
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
-  attribute { name = "LockID"  type = "S" }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
   tags = { Purpose = "terraform-lock" }
 }
