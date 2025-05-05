93224c7c39be7ce06b639e7ae4bcfc2aaf2cc7a4
kozraf <kozraf@gmail.com>
2025-05-05 12:46:25 +0200

    Install the AWS EBS CSI Driver

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,26 +3,32 @@ set -e
 
 export KUBECONFIG=/root/.kube/config
 
-# kubectl config view --minify # debug
-# kubectl get ns #debug
-
-# Add Helm repos
-
+# Add and update Helm repos
+helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add kubecost https://kubecost.github.io/cost-analyzer/
 helm repo add podinfo https://stefanprodan.github.io/podinfo
 helm repo update
 
-# Deploy Prometheus stack
-#helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
-#  --namespace monitoring --create-namespace
+# Install the AWS EBS CSI Driver and wait until ready
+helm upgrade --install aws-ebs-csi-driver aws-ebs-csi-driver/aws-ebs-csi-driver \
+  --namespace kube-system --create-namespace \
+  --set controller.serviceAccount.create=true \
+  --set controller.serviceAccount.name=ebs-csi-controller-sa \
+  --set defaultStorageClass.enabled=true \
+  --wait
 
-# Deploy Kubecost
+# (Optional) Deploy Prometheus stack
+# helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+#   --namespace monitoring --create-namespace
+
+# Deploy Kubecost and wait for pods to be ready
 helm --kubeconfig "$KUBECONFIG" upgrade --install kubecost kubecost/cost-analyzer \
      --namespace kubecost --create-namespace \
-     --set kubecostToken="demo"
+     --set kubecostToken="demo" \
+     --wait --timeout 5m
 
-# Deploy sample pod (podinfo)
-#helm upgrade --install podinfo podinfo/podinfo \
-#  --namespace apps --create-namespace
+# (Optional) Deploy sample pod
+# helm upgrade --install podinfo podinfo/podinfo \
+#   --namespace apps --create-namespace
